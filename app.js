d9063fffeaabe7b56582904c12eb380762e25ed2
Dhia Eddine <dhiaeddine214@gmail.com>
2020-04-17 17:55:03 +0100

    fixed class properites syntax babel

--- app.js
+++ app.js
@@ -4,31 +4,47 @@ import $ from 'jquery';
 import './app.css';
 // Component to inject on select
 class App extends Component {
-	sendSelection() {
-		$.post("http://localhost:4200",
-		{
-			term: this.props.selection
-		},
-		(data, status) => {
-			alert("Data: " + data + "\nStatus: " + status);
-		});
-	}
-	testapi() {
-		console.log('not yet');
-		$.get('http://localhost:4200', (data, status) => {
-			console.log(data);
-		});
-	}
-	componentDidMount() {
-		console.log('this props', this.props.selection);
-		this.sendSelection();
-	}
-	render() {
-		return <div className="result btn btn-primary">Primary</div>
-	}
+  loaded = false;
+  data;
+  sendSelection() {
+    $.post(
+      'http://localhost:4200',
+      {
+        term: this.props.selection,
+      },
+      (data, status) => {
+        console.log('data');
+        this.loaded = true;
+        this.data = data;
+        // alert('Data: ' + data + '\nStatus: ' + status);
+      }
+    );
+  }
+  testapi() {
+    console.log('not yet');
+    $.get('http://localhost:4200', (data, status) => {
+      console.log(data);
+    });
+  }
+  componentDidMount() {
+    console.log('this props', this.props.selection);
+    this.sendSelection();
+  }
+  render() {
+    let result;
+    if (!this.loaded) {
+      result = (
+        <div className="loading btn btn-sm btn-primary">Vérification</div>
+      );
+    }
+    return (
+      <div className="container">
+        {/* <div className="result btn btn-primary">Primary</div> */}
+        {result}
+      </div>
+    );
+  }
 }
-
-
 
 window.addEventListener('load', function () {
   console.log('loaded');
@@ -36,29 +52,29 @@ window.addEventListener('load', function () {
   // const watch = document.getElementById('target-test');
   $(function () {
     $(document.body).bind('mouseup', function (e) {
-			let selection;
-			let parentNode;
+      let selection;
+      let parentNode;
       if (window.getSelection) {
-				selection = window.getSelection();
-				if (selection.rangeCount > 0) {
-					parentNode = selection.getRangeAt(0).startContainer.parentNode;
-				}
+        selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+          parentNode = selection.getRangeAt(0).startContainer.parentNode;
+        }
       } else if (document.selection) {
-				selection = document.selection.createRange();
-				parentNode = document.selection.createRange().parentElement();
-			}
-			if (selection && selection.toString() !== '') {
-				app(selection.toString(), parentNode);
-			}
+        selection = document.selection.createRange();
+        parentNode = document.selection.createRange().parentElement();
+      }
+      if (selection && selection.toString() !== '') {
+        app(selection.toString(), parentNode);
+      }
       // selection.toString() !== '' &&
-        // alert(
-        //   '"' +
-        //     selection.toString() +
-        //     '" was selected at ' +
-        //     e.pageX +
-        //     '/' +
-        //     e.pageY
-        // );
+      // alert(
+      //   '"' +
+      //     selection.toString() +
+      //     '" was selected at ' +
+      //     e.pageX +
+      //     '/' +
+      //     e.pageY
+      // );
     });
   });
   // // Creates a new Mutation Observer
@@ -81,7 +97,7 @@ window.addEventListener('load', function () {
 
 const app = (selection, target) => {
   // observer.disconnect();
-	console.log('app funxtion executed');
+  console.log('app funxtion executed');
   if (!document.getElementById('react-root-test')) {
     const parent = target.parentNode;
     const root = document.createElement('div');
@@ -90,7 +106,7 @@ const app = (selection, target) => {
     parent.insertBefore(root, target);
 
     ReactDOM.render(
-      <App selection={selection}/>,
+      <App selection={selection} />,
       document.getElementById('react-root-test')
     );
   }
